fe056ce8f19342ef50c91e54184eb706ece4cd24
Olaf Schüsler <o.m.schusler@tudelft.nl>
2022-12-05 07:52:09 +0000

    [QI2-216] Create documentation for Compute API Client

--- generate.sh
+++ generate.sh
@@ -7,3 +7,12 @@ openapi-generator-cli generate \
     --library asyncio \
     --type-mappings enum=Enum
 
+mv compute_api_client_README.md README.md
+
+rm -rf ../../../docs/design/code/components/compute_api_client
+mkdir ../../../docs/design/code/components/compute_api_client
+cp README.md ../../../docs/design/code/components/compute_api_client/index.md
+cp compute_api_client/docs/* ../../../docs/design/code/components/compute_api_client/
+
+sed -i '' -e 's|compute_api_client/docs/||g' ../../../docs/design/code/components/compute_api_client/index.md
+find ../../../docs/design/code/components/compute_api_client -name "*.md" -exec sed -i '' -e 's|../README|index|g' {} \;
