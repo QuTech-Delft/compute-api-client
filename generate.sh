7b16606d992bdfe97f078e738b7d64f2dd8ba2c7
Olaf Schüsler <o.m.schusler@tudelft.nl>
2022-12-09 14:00:11 +0000

    [QI2-263] Update changelog for release 0.2

--- generate.sh
+++ generate.sh
@@ -7,6 +7,9 @@ openapi-generator-cli generate \
     --library asyncio \
     --type-mappings enum=Enum
 
+mkdir -p ../../../docs/design/code/interfaces/compute_job_manager
+curl -o ../../../docs/design/code/interfaces/compute_job_manager/openapi.json http://localhost:8000/openapi.json
+
 mv compute_api_client_README.md README.md
 
 rm -rf ../../../docs/design/code/components/compute_api_client
@@ -14,5 +17,5 @@ mkdir ../../../docs/design/code/components/compute_api_client
 cp README.md ../../../docs/design/code/components/compute_api_client/index.md
 cp compute_api_client/docs/* ../../../docs/design/code/components/compute_api_client/
 
-sed -i -e 's|compute_api_client/docs/||g' ../../../docs/design/code/components/compute_api_client/index.md
+sed -i '' -e 's|compute_api_client/docs/||g' ../../../docs/design/code/components/compute_api_client/index.md
 find ../../../docs/design/code/components/compute_api_client -name "*.md" -exec sed -i -e 's|../README|index|g' {} \;
