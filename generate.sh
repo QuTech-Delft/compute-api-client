1e794b610bf73c7d3754b357dd23b029f9f50a7c
Olaf Schüsler <o.m.schusler@tudelft.nl>
2023-09-05 12:07:20 +0000

    [QI2-523] Rearrange C4 code documentation

--- generate.sh
+++ generate.sh
@@ -9,18 +9,17 @@ openapi-generator-cli generate \
     --library asyncio \
     --type-mappings enum=Enum
 
-mkdir -p ../../../docs/design/code/interfaces/compute_job_manager
-curl -o ../../../docs/design/code/interfaces/compute_job_manager/openapi.json http://localhost:8000/openapi.json
+curl -o ../../../docs/design/code/apps/compute_job_manager_openapi.json http://localhost:8000/openapi.json
 
 mv compute_api_client_README.md README.md
 
-rm -rf ../../../docs/design/code/components/compute_api_client
-mkdir ../../../docs/design/code/components/compute_api_client
-cp README.md ../../../docs/design/code/components/compute_api_client/index.md
-cp compute_api_client/docs/* ../../../docs/design/code/components/compute_api_client/
+rm -rf ../../../docs/design/code/libs/compute_api_client
+mkdir ../../../docs/design/code/libs/compute_api_client
+cp README.md ../../../docs/design/code/libs/compute_api_client/index.md
+cp compute_api_client/docs/* ../../../docs/design/code/libs/compute_api_client/
 
-sed -i -e 's|compute_api_client/docs/||g' ../../../docs/design/code/components/compute_api_client/index.md
-find ../../../docs/design/code/components/compute_api_client -name "*.md" -exec sed -i -e 's|../README|index|g' {} \;
+sed -i -e 's|compute_api_client/docs/||g' ../../../docs/design/code/libs/compute_api_client/index.md
+find ../../../docs/design/code/libs/compute_api_client -name "*.md" -exec sed -i -e 's|../README|index|g' {} \;
 
 # Update the version of the poetry project
 git_tag=$(git describe --tags --abbrev=0)
